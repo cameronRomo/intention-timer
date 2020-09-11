d1587720fc60c82f110177bad243178a1b5ff5fa
Scott Brabson <scottbrabson@gmail.com>
2020-09-11 12:04:44 -0600

    Fix bug that stopped checking for empty goal input - now checks for empty string and returns error if empty

--- main.js
+++ main.js
@@ -11,20 +11,31 @@ var dataModel = {
 
 // EVENT LISTENERS 👇
 
-
 document.querySelector(".activities__icons-section").addEventListener('click', function(event) {
-  if (event.currentTarget.id !== undefined) {      // // TODO currentTarget or target?
-
-  selectCategory(event.target);
+  if (event.currentTarget.id !== undefined) {  // to avoid calling fxn w/click empty space (currentTarget or target?)
+    selectCategory(event.target);
   }
-  dataModel.category = ""  // add .value to object model
+});
+
+document.querySelector("#minutes-seconds-block").addEventListener('keydown', function(event) {
+  var validChars = [8,9,13,18,92,92,93];  //  keys like tab, etc
+  var selectedTimeInput = event.target.value;
+
+  if (event.keyCode >= 48 && event.keyCode <= 57 || validChars.includes(event.keyCode) === true) {
+    console.log(event.keyCode);
+    var number = KeyboardEvent.key.slice(-1);
+    selectedTimeInput += number;
+  } else {
+    alert("ONLY NUMBERS");
+    event.preventDefault();
+  }
+      // TODO visual/text indicator
 });
 
 document.querySelector(".activities__start-button").addEventListener('click', function() {
-  errorCheck();
-  // TODO check all inputs have values
-  // TODO function check make sure min/sec are numbers
-  // TODO submit to data model
+  goalCheck();  // for empty string
+  // collect data from all fields
+  // if min or sec empty = 0
   // TODO start timer
 });
 
@@ -47,31 +58,31 @@ function clearOtherCategories(el) {
   }
 };
 
-
-
-//working on ordering
-document.querySelector("#minutes-seconds-block").addEventListener('keydown', function(event) {
-  var validChars = [8,9,13,18,92,92,93];  //  keys like tab, etc
-  var selectedTimeInput = event.target.value;
-
-  if (event.keyCode >= 48 && event.keyCode <= 57 || validChars.includes(event.keyCode) === true) {
-    console.log(event.keyCode);
-    var number = KeyboardEvent.key.slice(-1);
-    selectedTimeInput += number;
+function goalCheck() {
+  var goals = document.querySelector("#goals-input");
+  if (!goals.value.trim().length === true) {
+    alert("Why you do dat?!?! ¯\_( ͡° ͜ʖ ͡°)_/¯")
+    //  display error "description required" + icon below field
+    return
   } else {
-    alert("ONLY NUMBERS");
-    event.preventDefault();
+    dataModelCollect("description")
   }
-      // TODO visual/text indicator
-});
+}
 
-// function goalCheck() {
-//   var goals = document.querySelector("#goals-input");
-//   if (!goals.trim().length === true) {
-//     alert("Why you do dat?!?! ¯\_( ͡° ͜ʖ ͡°)_/¯")
-//     //  display error "description required" + icon below field
-//     return
-//   } else {
-//      push goal into datamodel
-//   }
-// }
+function dataModelCollect(data) {
+
+
+  dataModel.description = goal
+}
+//
+// category: "",
+// description: "",
+// minutes: "",
+// seconds: "",
+// completed: false,
+// id:
+
+//
+// timer ends
+// instantiate class object
+// clear fields
