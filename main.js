1435769ae4fc4670c0e41c5190d4007081d5161d
Scott Brabson <scottbrabson@gmail.com>
2020-09-11 15:46:18 -0600

    Fix CSS bug regarding selection of only one icon with visual feedback

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ var dataModel = {
 // EVENT LISTENERS 👇
 
 document.querySelector(".activities__icons-section").addEventListener('click', function(event) {
-  if (event.currentTarget.id !== undefined) {
+  if (event.currentTarget.id !== undefined || null) {
     selectCategory(event.target);
   }
 });
@@ -28,30 +28,30 @@ document.querySelector("#minutes-seconds-block").addEventListener('keypress', fu
     alert("ONLY NUMBERS");
     event.preventDefault();
   }
-      // TODO function visual/text indicator
+  // TODO function visual/text indicator
 });
 
 document.querySelector(".activities__start-button").addEventListener('click', function() {
   descriptionCheck();
-  // for empty string
-  // collect data from all fields
-  // if min or sec empty = 0
   // TODO start timer
 });
 
 // EVENT HANDLERS 👇
 
 function selectCategory(category) {
-  category.classList.add(`${category.id}--active`);
+  // category.classList.add(`${category.id}-icon--active`);
+  document.querySelector(`#${category.id}`).classList.add(`${category.id}-icon--active`);
   dataModelCollect(category);
   clearOtherCategories(category);
 };
+
+// target icon block with ${ctagory.id}
 
 function clearOtherCategories(category) {
   var allCategories = document.querySelectorAll(".activities__figure");
   for (var i = 0; i < allCategories.length; i++) {
     if (allCategories[i].id !== category.id) {
-      allCategories[i].classList.remove(`${allCategories[i].id}--active`)       // TODO remove class from p tag in activities__figure child node
+      allCategories[i].classList.remove(`${allCategories[i].id}-icon--active`)       // TODO remove class from p tag in activities__figure child node
     }
   }
 };
@@ -90,7 +90,6 @@ function minuteSecondCheck(element) {
   }
 }
 
-//
 // timer ends
 // instantiate class object
 // clear fields
